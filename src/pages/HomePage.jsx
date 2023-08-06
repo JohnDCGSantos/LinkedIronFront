9dc1444c7b790e44940ca3e5519f704c5113fadb
JohnDCGSantos <jdcg.santos@gmail.com>
2023-08-06 22:59:41 +0100

    merge

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,9 @@
 const HomePage = () => {
-    return ( 
-        <div>
-           <h1>HomePage</h1>
-        
-        </div>
-       
-     );
+  return (
+    <div>
+      <h1>HomePage</h1>
+    </div>
+  )
 }
 
-export default HomePage;
+export default HomePage
