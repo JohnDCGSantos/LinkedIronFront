d0f24de218efc7ac98630ee46e36c0d027fdc7f0
nunoR <nunoribeiroft@outlook.com>
2023-08-07 11:06:40 +0100

    not showing post on feed

--- src/components/PostItem.jsx
+++ src/components/PostItem.jsx
@@ -5,30 +5,46 @@ import PostForm from './PostForm'
 
 function PostItem () {
     const [posts, setPosts] = useState([])
-
+    const [loading, setLoading] = useState(true)
+    const [error, setError] = useState(null)
+    const [likes, setLikes] = useState(0)
     const fetchAllPosts = async () =>{
         try {
+            const token = localStorage.getItem('authToken')
             const response = await axios.get(
-                `http://localhost:5005/posts/posts`)
-        const allPosts = response.data
-        setPosts(response.data)
+                `http://localhost:5005/posts/posts`, {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                  },
+                })
+                const allPosts = response.data
+                setPosts(allPosts)
+        setLoading(false)
+        
+        
         console.log(allPosts)
         } catch (error) {
             console.log('Error fetching all posts', error)
+            setLoading(false)
         }
     }
-    const handleNewPost = newPost => {
-        setPosts([...posts, newPost])
-      }
 
     useEffect(() =>{
         fetchAllPosts();
     }, [])
 
+    if (loading) {
+        return <p>Loading posts...</p>
+      }
+      if (error) {
+        return <p>{error}</p>
+      }
+    
     return(
         <>
-         <PostForm onNewPost={handleNewPost} />
+         
         {posts.map(post =>{
+            return(
         <div key={post._id}>
         <img src={post.image_url} alt='beerImg' style={{width: '70px'}} />
         <h1>{post.author}</h1>
@@ -36,10 +52,11 @@ function PostItem () {
         <p>{post.content}</p>
         <h1>{post.category}</h1>
         <p>{post.createdAt}</p>
-        <button onClick={() => setLikes(likes + 1)}>
+        {/* <button onClick={() => setLikes(likes + 1)}>
       {likes} Likes </button>
-        <Link to={`/posts/${post._id}`} >Check the details</Link>
-    </div>
+        <Link to={`/posts/${post._id}`} >Check the details</Link> */}
+        </div>
+        )
 
     })}
         </>
--- src/pages/Feed.jsx
+++ src/pages/Feed.jsx
@@ -1,6 +1,11 @@
+import PostItem from '../components/PostItem'
+
 function Feed(){
     return(
+        <div>
         <h1>Welcome to your Feed</h1>
+        <PostItem />
+        </div>
     )
 }
 export default Feed;
