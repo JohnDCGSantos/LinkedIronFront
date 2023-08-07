bae520ac2f59d3051050be31d3f83a285512e738
JohnDCGSantos <jdcg.santos@gmail.com>
2023-08-07 10:54:06 +0100

    bug c bar fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,66 +1,64 @@
-import { Link } from "react-router-dom";
-import { useContext } from "react";
-import { AuthContext } from "../context/Auth.context";
+import { Link } from 'react-router-dom'
+import { useContext } from 'react'
+import { AuthContext } from '../context/Auth.context'
 
 const Navbar = () => {
   // Access isLoggedIn state from the AuthContext
-  const { isLoggedIn, user, logOutUser } = useContext(AuthContext);
+  const { isLoggedIn, user, logOutUser } = useContext(AuthContext)
 
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-      <div className="container">
-        <Link className="navbar-brand" to={"/"}>
+    <nav className='navbar navbar-expand-lg navbar-dark bg-dark fixed-top'>
+      <div className='container'>
+        <Link className='navbar-brand' to={'/'}>
           LinkdIron
         </Link>
         <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
+          className='navbar-toggler'
+          type='button'
+          data-bs-toggle='collapse'
+          data-bs-target='#navbarNav'
+          aria-controls='navbarNav'
+          aria-expanded='false'
+          aria-label='Toggle navigation'
         >
-          <span className="navbar-toggler-icon"></span>
+          <span className='navbar-toggler-icon'></span>
         </button>
 
         {isLoggedIn ? (
           <>
-            <div className="collapse navbar-collapse" id="navbarNav">
-              <ul className="navbar-nav  ml-auto">
-                <li className="nav-item">
-                  <Link
-                    className="nav-link active"
-                    aria-current="page"
-                    to={"/Feed"}
-                  >
+            <div className='collapse navbar-collapse' id='navbarNav'>
+              <ul className='navbar-nav  ml-auto'>
+                <li className='nav-item'>
+                  <Link className='nav-link active' aria-current='page' to={'/Feed'}>
                     Feed
                   </Link>
                 </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to={"/Profile"}>
+                <li className='nav-item'>
+                  <Link className='nav-link' to={'/Profile'}>
                     Profile
                   </Link>
                 </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to={'/PostDetails'}>Create New Post</Link>
+                <li className='nav-item'>
+                  <Link className='nav-link' to={'/PostDetails'}>
+                    Create New Post
+                  </Link>
                 </li>
               </ul>
             </div>
-            <span className="navbar-text">
-              <div className="userStatus">
+            <span className='navbar-text'>
+              <div className='userStatus'>
                 <p>Hi {user.username}!</p>
                 <button onClick={logOutUser}>Logout</button>
               </div>
             </span>
           </>
         ) : (
-          <div className="navbar-text">
-            <div className="userStatus">
-              <Link to={"/Login"}>
+          <div className='navbar-text'>
+            <div className='userStatus'>
+              <Link to={'/Login'}>
                 <button>Login</button>
               </Link>
-              <Link c to={"/Signup"}>
+              <Link to={'/Signup'}>
                 <button>Signup</button>
               </Link>
             </div>
@@ -68,7 +66,7 @@ const Navbar = () => {
         )}
       </div>
     </nav>
-  );
-};
+  )
+}
 
-export default Navbar;
+export default Navbar
