f4093cf287e89e07122cbb52b4507a0496ed526d
JohnDCGSantos <jdcg.santos@gmail.com>
2023-08-08 12:17:22 +0100

    followpage

--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState } from 'react'
+/*import { useContext, useEffect, useState } from 'react'
 import axios from 'axios'
 import FollowButton from '../components/FollowButton'
 import { AuthContext } from '../context/Auth.context'
@@ -64,15 +64,19 @@ function UsersList() {
   )
 }
 
-export default UsersList
+export default UsersList;*/
 
-/*import { useEffect, useState } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import axios from 'axios'
 import FollowButton from '../components/FollowButton'
+import { AuthContext } from '../context/Auth.context'
+import '../Users.css'
 
 function UsersList() {
   const [users, setUsers] = useState([])
   const [isLoading, setIsLoading] = useState(true)
+  const [searchQuery, setSearchQuery] = useState('')
+  const authContext = useContext(AuthContext) // Use the AuthContext directly
 
   useEffect(() => {
     const fetchUsers = async () => {
@@ -95,25 +99,42 @@ function UsersList() {
     fetchUsers()
   }, [])
 
+  const handleFollow = followUserId => {
+    // Logic to handle follow action
+    console.log(`Followed user ${followUserId}`)
+  }
+
+  const handleUnfollow = followUserId => {
+    // Logic to handle unfollow action
+    console.log(`Unfollowed user ${followUserId}`)
+  }
   if (isLoading) {
     return <div>Loading...</div>
   }
+  const filteredUsers = users.filter(
+    user =>
+      user._id !== authContext.user._id &&
+      user.username.toLowerCase().includes(searchQuery.toLowerCase()) // Filter users by search query
+  )
 
   return (
-    <div>
+    <div className='users-list-container'>
       <h2>Users List</h2>
-      <ul>
-        {users.map(user => (
-          <li key={user._id}>
+      <input
+        type='text'
+        placeholder='Search users by name'
+        value={searchQuery}
+        onChange={e => setSearchQuery(e.target.value)}
+      />
+      <ul className='users-list'>
+        {filteredUsers.map(user => (
+          <li key={user._id} className='user-item'>
             {user.username}
             <FollowButton
-              userId={user._id} // Replace this with the ID of the currently logged-in user
-              onFollow={() => {
-                console.log(`Followed user ${user._id}`)
-              }}
-              onUnfollow={() => {
-                console.log(`Unfollowed user ${user._id}`)
-              }}
+              userId={authContext.user._id}
+              followUserId={user._id}
+              onFollow={() => handleFollow(user._id)}
+              onUnfollow={() => handleUnfollow(user._id)}
             />
           </li>
         ))}
@@ -122,4 +143,4 @@ function UsersList() {
   )
 }
 
-export default UsersList*/
+export default UsersList
